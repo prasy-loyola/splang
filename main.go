b09b7ac6b34d51fd5e6cdd512ce07d993df427ec
Prasanna Selvaraj <prasy.loyola@gmail.com>
2023-08-09 17:24:05 +0800

    implement interpreter for plus

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 type TokenType int32;
@@ -49,9 +50,10 @@ func tokenize(lexer *Lexer) error {
             pos: lexer.position,
             literal: "",
         }
-        for ; char >= '0' && char <= '9' ; lexer.position++ {
+        for ; lexer.text[lexer.position] >= '0' && lexer.text[lexer.position] <= '9' ; lexer.position++ {
             char = lexer.text[lexer.position]
             token.literal = token.literal + string(char)
+            
         }
 
         lexer.tokens = append(lexer.tokens, token)
@@ -70,22 +72,78 @@ func tokenize(lexer *Lexer) error {
             literal: string(char),
         }
         lexer.tokens = append(lexer.tokens, token)
-        return errors.New("Illegal token") 
+        return errors.New("Illegal token " + string(char)) 
     }
-    fmt.Println(lexer)
+    //fmt.Println(lexer)
     return tokenize(lexer)
+}
+
+type Interpreter struct {
+    stack []int
+}
+
+func (i *Interpreter) interpret(lexer *Lexer) error {
+
+    fmt.Println("Got lexer",lexer)
+    fmt.Println("Got tokens",lexer.tokens)
+
+    for _ , token := range lexer.tokens {
+
+        fmt.Println("Got token", token)
+        if token.tokenType == Number {
+            num, err := strconv.Atoi(token.literal)
+            if err != nil {
+                fmt.Println(err)
+                return err
+            }
+            i.stack = append(i.stack, num)
+            fmt.Println("After adding number to stack", i.stack)
+        } else if token.tokenType == Plus {
+
+            if len(i.stack) < 2 {
+                errors.New("Too little items on the stack. Need at least two for plus operator")
+            }
+
+            a , b := i.stack[len(i.stack)-1] , i.stack[len(i.stack)-2]
+            i.stack = i.stack[:len(i.stack)-2]
+            i.stack = append(i.stack, a + b)
+            fmt.Println(i.stack)
+        } else {
+
+            fmt.Println(token)
+
+        }
+
+    }
+    return nil
+
 }
 
 func main() {
     lexer := Lexer { 
-        text : " 10 20 +" ,
+        text : " 10 20 + " ,
         position : 0,
         tokens: []Token{},
     }
+
+
 
     err := tokenize(&lexer)
     if err != nil {
         fmt.Println("inside error",lexer)
         fmt.Printf("err: %v\n", err)
+        return
     }
+
+    interpreter := Interpreter {
+        stack : []int{},
+    }
+    err = interpreter.interpret(&lexer)
+
+    if err != nil {
+        fmt.Println("inside error",lexer)
+        fmt.Printf("err: %v\n", err)
+        return
+    }
+    fmt.Println(interpreter)
 }
